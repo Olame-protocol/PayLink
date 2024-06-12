8545dd7109bd3bf12d39ab8aa9e2626a3a1546f9
Jonath-z <jonathanzihindula95@gmail.com>
2024-06-12 20:45:24 +0200

    fix: add margin to header and update yarn lock file

--- packages/react-app/components/LandinPage/Header.tsx
+++ packages/react-app/components/LandinPage/Header.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex min-h-[85vh] w-full flex-col bg-forest pb-[6rem]">
+    <div className="mt-28 flex min-h-[85vh] w-full flex-col bg-forest pb-[6rem]">
       <Section>
         <div className="mt-[62px] flex flex-col space-y-8 text-center md:mt-[126px] md:space-y-14">
           <div>
@@ -17,7 +17,9 @@ const Header = () => {
             <p className="text-xs text-green-petrolium md:text-2xl">Simplifying digital payments, one click at a time</p>
           </div>
           <div>
-            <Link href="/payments" className="rounded-lg bg-white px-10 py-4 text-sm font-semibold text-forest md:text-xl">Generate a link</Link>
+            <Link href="/payments" className="rounded-lg bg-white px-10 py-4 text-sm font-semibold text-forest md:text-xl">
+              Generate a link
+            </Link>
           </div>
           <div className="relative">
             <div className="absolute -right-4 -top-5 md:-right-10 md:-top-20">
