75a3b4fcc267ae7b5644dcbf54e8ee0e49ef0a6a
Jonath-z <jonathanzihindula95@gmail.com>
2024-07-11 16:00:10 +0200

    chore: refactor and add twitter link

--- packages/react-app/components/LandinPage/Footer.tsx
+++ packages/react-app/components/LandinPage/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Section from "../Section";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaTelegram, FaXTwitter } from "react-icons/fa6";
 import { BsGithub } from "react-icons/bs";
 
 const Footer = () => {
@@ -14,7 +14,10 @@ const Footer = () => {
           <p className="flex items-center gap-x-1 text-[10px] lg:gap-x-2 lg:text-base">© 2023 - {currentYear} Paylink</p>
         </div>
         <div className="flex items-center gap-x-2 lg:gap-x-7">
-          <a className="cursor-pointer" href="https://x.com/paylinkdapp?s=11" target="_blank">
+          <a className="cursor-pointer" href="https://x.com/paylinkme" target="_blank">
+            <FaTelegram className="lg:h-6 lg:w-6" />
+          </a>
+          <a className="cursor-pointer" href="https://x.com/paylinkme" target="_blank">
             <FaXTwitter className="lg:h-6 lg:w-6" />
           </a>
           <a className="cursor-pointer" href="/">
